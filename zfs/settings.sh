722efa19757f7096bfdaa475a83c7ce3eefdbb1b
elvout <eyang@cs.utexas.edu>
2021-05-15 20:38:40 -0500

    Update zfs init settings

--- zfs/settings.sh
+++ zfs/settings.sh
@@ -2,20 +2,39 @@
 
 zpool create \
 	-o ashift=12 \
+	-o listsnapshots=on \
 	-O atime=off \
 	-O compression=lz4 \
 	<pool> <vdevs>
 
-zpool set listsnapshots=on <pool>
+# if initializing on linux, disable features for O3X RW compatibility
+## features as of OpenZFS 0.8.3:
+	-o feature@project_quota=disabled \
+	-o feature@userobj_accounting=disabled \
 
+
+# if the vdevs use ssds
+zpool set autotrim=on <pool>
+
+# treat pool as a pure container
+zfs set canmount=off <pool>
 
 zfs create \
 	-o encryption=aes-128-gcm \
 	-o keyformat=passphrase \
 	-o keylocation=prompt \
-	-o pbkdf2iters=1000000 \
+	-o pbkdf2iters=1M \
 	<pool>/backups
 
 # inherits encryption
 zfs create <pool>/backups/<machine>
-zfs set quota= <pool>/backups/<machine>
+
+zfs set quota=<...> <pool>/backups/<machine>
+
+
+# disable spotlight indexing on macos
+## prevents index files from taking space in snapshots
+mdutil -X <abspath>
+touch <path>/.metadata_never_index
+## https://apple.stackexchange.com/a/81737
+touch <path>/.fseventsd/no_log
